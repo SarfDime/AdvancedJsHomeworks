b6765c64f228423126f69db08a00bf915f193e72
SarfDime <118685209+SarfDime@users.noreply.github.com>
2023-02-24 19:12:50 +0100

    stuff

--- getCountries/countries.js
+++ getCountries/countries.js
@@ -14,12 +14,5 @@ const getCountries = async (code) => {
     mainCountry[0].borders.forEach(async e => {
         let borderingCOuntry = await makeCall(`https://restcountries.com/v3.1/alpha/${e}`)
         console.log("Country : ", borderingCOuntry[0])
-
-        // console.log("Country : ", await makeCall(`https://restcountries.com/v3.1/alpha/${e}`))
-        // Zoshto ova ne se pokazhuva vo browser a raboti vo console ninja
-        // https://prnt.sc/vu7uJOx5_5JW browser
-        // https://prnt.sc/6HHu4FdQc8FP console ninja
     });
 }
-
-getCountries("mkd")
